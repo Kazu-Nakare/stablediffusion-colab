1ba34ea0d03b1be2212ac2478d45d186ad8ebe13
Kazu-Nakare <87691092+Kazu-Nakare@users.noreply.github.com>
2023-01-30 14:14:12 +0900

    採集

--- frontend/src/components/Generator.vue
+++ frontend/src/components/Generator.vue
@@ -2,33 +2,35 @@
   <v-container fluid class="fill-height align-stretch">
     <v-row>
       <v-col cols="4" class="d-flex flex-column">
-        <v-card class="mb-1">
+        <v-card class="mb-1 pa-2">
           <v-textarea outlined hide-details readonly :value="total_prompt">
           </v-textarea>
-          <v-btn icon v-clipboard:copy="total_prompt" v-clipboard:success="() => snackbar=true">
-            <v-icon>
-              mdi-clipboard-text-multiple-outline
-            </v-icon>
-          </v-btn>
-          <v-snackbar top color="green" v-model="snackbar" timeout="3000" transition="slide-y-transition">
-            Copyed!
-            <template v-slot:action="{ attrs }">
-              <v-btn icon v-bind="attrs" @click="snackbar = false">
-                <v-icon>
-                  mdi-close
-                </v-icon>
-              </v-btn>
-            </template>
-          </v-snackbar>
-          <v-progress-circular
-            v-if="loading"
-            size="24"
-            color="info"
-            indeterminate
-          ></v-progress-circular>
-          <v-btn v-else @click="generate">
-            Generate
-          </v-btn>
+          <div class="d-flex pt-1 justify-end">
+            <v-btn class="mr-2" elevation="4" v-clipboard:copy="total_prompt" v-clipboard:success="() => snackbar=true">
+              <v-icon>
+                mdi-clipboard-text-multiple-outline
+              </v-icon>
+            </v-btn>
+            <v-snackbar top color="green" v-model="snackbar" timeout="3000" transition="slide-y-transition">
+              Copyed!
+              <template v-slot:action="{ attrs }">
+                <v-btn icon v-bind="attrs" @click="snackbar = false">
+                  <v-icon>
+                    mdi-close
+                  </v-icon>
+                </v-btn>
+              </template>
+            </v-snackbar>
+            <v-progress-circular
+              v-if="loading"
+              size="24"
+              color="info"
+              indeterminate
+            ></v-progress-circular>
+            <v-btn v-else color="warning" elevation="4" @click="generate">
+              GENERATE
+            </v-btn>
+          </div>
         </v-card>
         <v-card class="mb-1 flex-grow-1 overflow-auto">
           <v-container>
@@ -51,24 +53,22 @@
                   </template>
                   <v-card class="d-flex flex-row pa-2">
                     <v-text-field
+                      dense
+                      outlined
+                      label="EMPHASIS"
                       v-model.number="tags[i].bracket"
                       type="number"
                       style="width: 60px"
-                      outlined
                       hide-details>
                     </v-text-field>
                     <v-text-field
-                      :value="tags[i].context"
+                      dense
                       outlined
+                      :value="tags[i].context"
+                      @change="value => tags[i].context = value"
                       hide-details
-                      label="Tag"
-                      placeholder="house"
-                      prepend-inner-icon="mdi-comment-text">
-                      <template v-slot:append-outer>
-                        <v-icon @click="log(tag)">
-                          mdi-send
-                        </v-icon>
-                      </template>
+                      label="TAG"
+                      placeholder="beautiful house">
                     </v-text-field>
                   </v-card>
                 </v-menu>
@@ -85,18 +85,28 @@
                   <v-icon>mdi-plus</v-icon>
                 </v-btn>
               </template>
-              <v-card class="pa-2">
+              <v-card class="d-flex flex-row pa-2">
                 <v-text-field
-                  v-model="newtag"
+                  dense
+                  outlined
+                  label="EMPHASIS"
+                  v-model="newtag.bracket"
+                  type="number"
+                  style="width: 60px"
+                  hide-details>
+                </v-text-field>
+                <v-text-field
+                  v-model="newtag.context"
+                  dense
                   outlined
                   hide-details
-                  label="New Tag"
+                  label="NEW TAG"
                   placeholder="A photo of an astronaut riding a horse"
                   clearable
                   clear-icon="mdi-close-circle"
                   prepend-inner-icon="mdi-comment-text">
                   <template v-slot:append-outer>
-                    <v-icon @click="log(newtag)">
+                    <v-icon @click="pushtag(newtag)">
                       mdi-send
                     </v-icon>
                   </template>
@@ -105,7 +115,7 @@
             </v-menu>
           </v-container>
         </v-card>
-        <v-card>
+        <v-card class="pa-2">
           <v-text-field
             v-model="newprompt"
             outlined
@@ -115,23 +125,31 @@
             clearable
             clear-icon="mdi-close-circle"
             prepend-inner-icon="mdi-comment-text"
-            :disabled="loading">
+            :disabled="loading"
+            class="d-flex align-end">
             <template v-slot:append-outer>
-              <v-btn @click="insert">
+              <v-btn color="info" @click="insert" large>
                 Insert
               </v-btn>
             </template>
           </v-text-field>
         </v-card>
       </v-col>
-      <v-col class="info">
-        <div v-for="image in images" :key="image">
-          <a :href="image.url" :download="image.total_prompt">
-            <v-img contain :src="image.url" :title="image.total_prompt"></v-img>
+      <v-col>
+        <v-card class="pa-2" height="100%">
+          <a :href="images.slice(-1)[0].url" :download="images.slice(-1)[0].total_prompt">
+            <v-img contain :src="images.slice(-1)[0].url" :title="images.slice(-1)[0].total_prompt"></v-img>
           </a>
-        </div>
+        </v-card>
       </v-col>
       <v-col cols="2" class="white">
+        <v-card class="pa-2" height="100%">
+          <div v-for="image in images" :key="image">
+            <a :href="image.url" :download="image.total_prompt">
+              <v-img contain :src="image.url" :title="image.total_prompt"></v-img>
+            </a>
+          </div>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -149,18 +167,18 @@ import draggable from 'vuedraggable'
     data () {
       return {
         newprompt: "",
-        newtag: "",
+        newtag: {bracket: 0, context: ""},
         images: [],
         loading: false,
         snackbar: false,
-        tags: [{bracket: -3, context: "aaaa"}]
+        tags: [{bracket: 3, context: "house"}]
       }
     },
     methods: {
-      log(tag) {
+      pushtag() {
         if (this.newtag) {
-          this.tags.push({bracket: 0, context: tag})
-          this.newtag = ""
+          this.tags.push(this.newtag)
+          this.newtag = {bracket: 0, context: ""}
         }
       },
       insert() {
@@ -169,7 +187,7 @@ import draggable from 'vuedraggable'
           let newtags = []
 
           prompt.forEach((pmt)=>{
-            const re_p = /\{(.+)\}/
+            const re_p = /\((.+)\)/
             const re_m = /\[(.+)\]/
             let cnt = 0
             while (pmt.match(re_p) || pmt.match(re_m)) {
@@ -191,7 +209,7 @@ import draggable from 'vuedraggable'
       prom_gen(tag) {
         let result = tag.context
         if (tag.bracket > 0) {
-          result = "{".repeat(tag.bracket) + result + "}".repeat(tag.bracket)
+          result = "(".repeat(tag.bracket) + result + ")".repeat(tag.bracket)
         } else if (tag.bracket < 0) {
           result = "[".repeat(-tag.bracket) + result + "]".repeat(-tag.bracket)
         }
